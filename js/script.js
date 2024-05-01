8ff247bd6d7b8c8e3244cea0c1b3fc1e5132956e
lam <i.mailed.lam@gmail.com>
2024-05-01 18:11:25 +0200

    GUess the word game part 1

---
+++ js/script.js
@@ -0,0 +1,34 @@
+const guessedLetters= document.querySelector(".guessed-letters");
+const guessButton = document.querySelector(".guess");
+const letterInput = document.querySelector(".letter");
+const wordInProgress = document.querySelector(".word-in-progress");
+const remainingGuesses = document.querySelector(".remaining");
+const remainingSpan = document.querySelector(".remaining span");
+const message = document.querySelector(".message");
+const playAgainButton =document.querySelector(".play-again");
+
+const word = "magnolia";
+
+//display your symbols as placeholders for the chosen words letters
+
+const placeholder = function (word) {
+    const placeholderLetters = [];
+    for (const letter of word) {
+        console.log(letter);
+        placeholderLetters.push("🕳️");
+    }
+    wordInProgress.innerText = placeholderLetters.join("");
+};
+
+placeholder(word);
+
+//adding event listener for the button
+
+guessButton.addEventListener("click",function (e) {
+    e.preventDefault();
+    const guess = letterInput.value;
+    console.log(guess);
+    letterInput.value;
+});
+
+
